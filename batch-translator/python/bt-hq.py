3a85ec59966dfae86f490e3b444960c327efa723
Binjian <binjian.tu@HQ.TA-MP.COM>
2017-08-07 14:47:46 +0800

    development update

--- batch-translator/python/bt-hq.py
+++ batch-translator/python/bt-hq.py
@@ -4,12 +4,61 @@ import xlrd
 #import xlutils
 #from xlutils.copy import copy as xlutils_copy
 from websocket_server import WebsocketServer
+from pptx import Presentation
+from docx import Document
 
-def read(file):
-    book = xlrd.open_workbook(file)
+class OpenXml:
+    def __init__(self, oxml, type, list):
+        self.openXml = oxml
+        self.list = list
+        self.type = type
+
+    def save(self, path):
+        self.openXml.save(path)
+
+def openDocument(path):
+    t = splitFileType(path)
+    if t == 'xls' or t == 'xlsx':
+        return openXLS(path)
+    elif t == 'pptx':
+        return openPPTX(path)
+    elif t == 'docx':
+        return openDOCX(path)
+    else:
+        print('NOT SUPPORTED')
+
+def openDOCX(path):
+    docx = Document(path)
+    paragraphs = docx.paragraphs
+    compound = []
+    for p in paragraphs:
+        if p.text.strip() != '':
+            compound.append((p.text, p))
+
+    return OpenXml(docx, 'docx', compound)
+
+def openPPTX(path):
+    prs = Presentation(path)
+    # text_runs will be populated with a list of strings,
+    # one for each text run in presentation
+    compound = []
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            for paragraph in shape.text_frame.paragraphs:
+                for run in paragraph.runs:
+                    if run.text.strip() != '':
+                        print(run.text)
+                        compound.append((run.text, run))
+
+    return OpenXml(prs, 'pptx', compound)
+
+def openXLS(path):
+    book = xlrd.open_workbook(path)
     #table = data.sheet_by_index(0)
     tables = book.sheets()
-    src = []
+    compound = []
 
     for table in tables:
         nRows = table.nrows
@@ -17,12 +66,13 @@ def read(file):
         for i in range(nRows):
             for j in range(nCols):
                 v = table.cell(i, j).value
-                v = v.strip()
-                if v != '':
-                    src.append(v)                    
-                    print(table.cell(i, j).value)
+                if v.strip() != '':
+                    element = table.cell(i, j)
+                    #src.append(v)
+                    compound.append((element.value, element))
+                    print(element.value)
 
-    return src
+    return OpemXml(book, 'xlsx', compound)
 
 def translateByChromeExtension(src):
     data = [0, src]
@@ -34,8 +84,9 @@ def translateByChromeExtension(src):
     def new_client(client, server):
         print("New client connected and was given id %d" % client['id'])
         data = new_client.data
-        if data[0] < len(data[1]) :
-            server.send_message(client, data[1][data[0]])
+        if data[0] < len(data[1]):
+            #server.send_message(client, data[1][data[0]])
+            server.send_message(client, data[1][data[0]][0])
             data[0] += 1
         else:
             print('finished!')
@@ -52,11 +103,18 @@ def translateByChromeExtension(src):
     def message_received(client, server, message):
         #if len(message) > 200:
 	    #    message = message[:200]+'..'
-        print("<== Client(%d) said: %s" % (client['id'], message))
+        #print("<== Client(%d) said: %s" % (client['id'], message))
         data = message_received.data
-        print('    ==> ' + data[1][data[0]])
-        if data[0] < len(data[1]) :
-            server.send_message(client, data[1][data[0]])
+        
+        #print('==> ' + data[1][data[0]])
+        print('==>:' + data[1][data[0] - 1][0])
+        print("<==:" + message)        
+
+        data[1][data[0] -1][1].text = message
+        
+        if data[0] < len(data[1]):
+            #server.send_message(client, data[1][data[0]])
+            server.send_message(client, data[1][data[0]][0])
             data[0] += 1
         else:
             print('finished!')
@@ -71,10 +129,16 @@ def translateByChromeExtension(src):
     server.set_fn_message_received(message_received)
     server.run_forever()
 
+def splitFileType(f):
+    t = f[f.rfind('.') + 1 :]
+    return t
+
 if __name__ == '__main__':
-    file = sys.path[0] + '/' + sys.argv[1]
-    source = read(file)
-    if len(source) <= 0:
-        print('data source out of range.')
+    path = sys.path[0] + '/' + sys.argv[1]
+    openxml = openDocument(path)
+    #source = readXLS(path)
+    #if len(source) <= 0:
+    #    print('data source out of range.')
     
-    translateByChromeExtension(source)
+    translateByChromeExtension(openxml.list)
+    openxml.save(sys.argv[1] + 'dest.' + openxml.type)
