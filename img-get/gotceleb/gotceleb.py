b40f0fb008d60e8a0cece4972462ec624dcf9123
Binjian <binjian.tu@HQ.TA-MP.COM>
2018-06-02 17:04:40 +0800

    update 06021704

--- img-get/gotceleb/gotceleb.py
+++ img-get/gotceleb/gotceleb.py
@@ -7,26 +7,135 @@ import sys
 import time
 import os
 import json
-import random
 import http.client
+
+if sys.version.find('3') == 0:
+    import urllib.request
+    urlrequest = urllib.request
+    urlparse = urllib.parse
+else:
+    import urllib2
+    urlrequest = urllib2
+    urlparse = urllib
 
 if __name__ == '__main__':
     print('gotceleb do nothing as main')
 
 def current():
-    return sys.path[0] + '/'
+    return os.path.dirname(os.path.realpath(__file__)) + '/'
 
-def get_keyword(keyword):
-    f = open(current() + keyword, 'r')
-    keywords = f.read()
-    print(keywords)
+def get_keyword(file_path):
+    f = open(file_path, 'r')
+    content = f.read()
     f.close()
-    return keywords
+    keywords = content.split(',')
+    ret = []
+    for word in keywords:
+        word = word.strip('\u0020\u3000\n')
+        ret.append(re.sub('[\u3000|\u0020]+', '+', word))
+
+    last = ret[-1]
+    if None is last or 0 == len(last):
+        ret.remove(last)
+        
+    print(ret)
+    return ret
+
+def findLink(content, key, beg):
+    beg = content.find(key, beg)
+    if -1 == beg:
+        print('beg is -1?')
+        return
+    beg = beg + len(key)
+    end = content.find('"', beg)
+    if -1 == end:
+        print('end is -1?')
+        return
+    return content[beg:end]
+
+def findHref(content, beg):
+    return findLink(content, 'href="', beg)
+
+def findSrc(content, beg):
+    return findLink(content, 'src="', beg)
+
+def getName(url):
+    beg = url.rfind('/')
+    return url[beg:]
 
 class Fetch:
     def __init__(self):
-        keywords = get_keyword(current() + 'keyword-list.txt')
-        keywords = keywords.split(',')
+        self.keywords = get_keyword(current() + 'keyword-list.txt')
+        #self.homePage = 'http://www.baidu.com'
+        self.homePage = 'http://www.gotceleb.com/?s='
+        print('gotceleb.Fetch init')
+
+    def common(self, url, h):
+        crawler = self.crawler
+        print(url)
+        req = urlrequest.Request(url = url, headers = h)
+        content = crawler.read(crawler.visit(req))
+        return content
+
+    def second(self, url, h):
+        content = self.common(url, h)
+
+        '''
+        key = 'class="attachment-medium size-medium"'
+        beg = content.find(key)
+        pic = findSrc(content, beg + len(key))
+        req = urlrequest.Request(url = pic, headers = h)
+        print('ok4')
+        crawler.download(req, current() + getName(pic))
+        '''
+        key = 'class="gallery-icon portrait"'
+        bigs = content.split(key)
+        print('OK3')
+
+        ret = []
+        for big in bigs:
+            url = findHref(big, 0)
+            if None is url:
+                print('href url is None? NG3-1.')
+                continue
+            ret.append(url)
+
+        return ret
+
+    def third(self, url, h):
+        crawler = self.crawler
+        content = self.common(url, h)
+        key = 'class="attachment-medium size-medium"'
+        beg = content.find(key)
+        pic = findSrc(content, beg + len(key))
+        if None is pic:
+            print('pic is None? NG4')
+            return
+        req = urlrequest.Request(url = pic, headers = h)
+        print('OK4')
+        crawler.download(req, current() + getName(pic))
+        
+    def begin(self, crawler):
+        self.crawler = crawler
+        for word in self.keywords:
+            h = crawler.getHeaders()
+            content = self.common(self.homePage + word, h)
+            #req = urlrequest.Request(url = self.homePage + word, headers = h)
+            #res = crawler.visit(req)
+            #content = crawler.read(res)
+            articles = content.split('article')
+            print('OK2')
+            for article in articles:
+                url = findHref(article, 0)
+                if None is url:
+                    continue
+                req = urlrequest.Request(url = url, headers = h)
+                content = crawler.read(crawler.visit(req))
+                bigs = self.second(crawler, url, h)
+                for big in bigs:
+                    self.third(big, h)
+                
+            #print(res.read())        
         
         
         
--- img-get/main.py
+++ img-get/main.py
@@ -24,7 +24,7 @@ def detectEncoding(res):
     return header[header.find('charset=') + len('charset='):]
 
 def current():
-    return sys.path[0] + '/'
+    return os.path.dirname(os.path.realpath(__file__)) + '/'
     
 class ImgCrawler:
     def __init__(self):
@@ -32,8 +32,75 @@ class ImgCrawler:
         handler = urlrequest.HTTPCookieProcessor(cookieJar)
         self.opener = urlrequest.build_opener(handler)
 
-    def setCore(self, core):
-        self.core = core
+    def assign(self, fetcher):
+        self.fetcher = fetcher
+
+    def visit(self, req):
+        return self.opener.open(req)
+
+    @staticmethod
+    def read(res):
+        failed = True
+        encode = detectEncoding(res)
+        b = res.read()
+        try:
+            #print('try original:'+ encode)
+            u = b.decode(encode, 'ignore')
+            failed = False
+        except Exception as e:
+            print(e)    
+
+        if failed == True:    
+            try:
+                print('original %s failed, try utf8'% encode)
+                u = b.decode('utf8')
+                failed = False
+            except Exception as e:
+                print(e)
+
+        if failed == True:
+            try:
+                print('try gbk')
+                u = b.decode('gbk')
+                failed = False
+            except Exception as e:
+                print(e)
+
+        if failed == True:
+            try:
+                print('try utf8 ignore')
+                u = b.decode('utf8', 'ignore')
+                failed = False
+            except Exception as e:
+                print(e)
+
+        if failed == True:
+            try:
+                print('try gbk ignore')
+                u = b.decode('gbk', 'ignore')
+                failed = False
+            except Exception as e:
+                print(e)
+                return
+            
+        '''
+        try:
+            ppu = u.encode('gbk', 'ignore')
+            print(ppu)
+        except Exception as e:
+            print(e)
+        '''
+            
+        return u
+
+    @staticmethod
+    def getHeaders():
+        h = {
+            'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0',
+            'Accept-Language' : 'en-US,en;q=0.5',
+            'Connection' : 'keep-alive',
+        }
+        return h
 
     @staticmethod
     def headers(update):
@@ -45,25 +112,30 @@ class ImgCrawler:
         h.update(update)
         return h
 
-    def write(self, b, fileName):
-        path = self.current
+    @staticmethod
+    def detectEncoding(res):
+        header = res.getheader('Content-Type').lower()
+        return header[header.find('charset=') + len('charset='):]
+
+    def write(self, b, path):
         if os.path.exists(path) == False:
             os.mkdir(path)
-        f = open(path + fileName, 'wb')
+        f = open(path, 'wb')
         f.write(b)
         f.flush()
         f.close()
         
-    def download(self, url, fileName):
+    def download(self, url, path):
         b = self.opener.open(url).read()
-        self.write(b, fileName)
+        self.write(b, path)
 
 def fetch_web_objects(webs):
     cur = current()
     f = open(cur + webs, 'r')
     objs = json.loads(f.read())#, encoding="UTF8")
-    print(objs.keys())
     f.close()
+    return (objs.keys())
+    
 
 def get_keyword(keyword):
     f = open(current() + keyword, 'r')
@@ -72,6 +144,18 @@ def get_keyword(keyword):
     f.close()
     return keywords
 
+def main():
+    crawler = ImgCrawler()
+    fetchers = fetch_web_objects('webs.json')
+    objs = []
+    for fetcher in fetchers:
+        exec('from %s import %s' % (fetcher, fetcher))
+        obj = eval('%s.Fetch()' % (fetcher))
+
+        obj.begin(crawler)
+        
+        objs.append(obj)
+        
+        
 if __name__ == '__main__':
-    fetch_web_objects('webs.json')
-    print(get_keyword('gotceleb/keyword-list.txt').split(','))
+    main()
